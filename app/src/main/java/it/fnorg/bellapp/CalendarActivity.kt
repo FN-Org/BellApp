949a31ebfcb8da89df943c366c7365723bf084fc
N1C03BN0 <s5182167@studenti.unige.it>
2024-06-24 17:17:54 +0200

    Commit a metà calendario

--- app/src/main/java/it/fnorg/bellapp/CalendarActivity.kt
+++ app/src/main/java/it/fnorg/bellapp/CalendarActivity.kt
@@ -6,14 +6,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.Week
+import com.kizitonwose.calendar.core.WeekDay
+import com.kizitonwose.calendar.core.atStartOfMonth
 import com.kizitonwose.calendar.core.daysOfWeek
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
-import com.kizitonwose.calendar.view.CalendarView
-import com.kizitonwose.calendar.view.MonthDayBinder
-import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
 import com.kizitonwose.calendar.view.ViewContainer
+import com.kizitonwose.calendar.view.WeekCalendarView
+import com.kizitonwose.calendar.view.WeekDayBinder
+import com.kizitonwose.calendar.view.WeekHeaderFooterBinder
+import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.Locale
@@ -23,44 +25,44 @@ class CalendarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar_activity_calendar)
 
-        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        val weekCalendarView: WeekCalendarView = findViewById(R.id.calendarView)
 
-        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+        weekCalendarView.dayBinder = object : WeekDayBinder<DayViewContainer> {
             // Called only when a new container is needed.
             override fun create(view: View) = DayViewContainer(view)
 
             // Called every time we need to reuse a container.
-            override fun bind(container: DayViewContainer, data: CalendarDay) {
+            override fun bind(container: DayViewContainer, data: WeekDay) {
                 container.textView.text = data.date.dayOfMonth.toString()
             }
         }
 
+        val currentDate = LocalDate.now()
         val currentMonth = YearMonth.now()
-        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
-        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
-        calendarView.setup(startMonth, endMonth, firstDayOfWeek)
-        calendarView.scrollToMonth(currentMonth)
-
-        val daysOfWeek = daysOfWeek() // Available in the library
+        val startDate = currentMonth.minusMonths(100).atStartOfMonth() // Adjust as needed
+        val endDate = currentMonth.plusMonths(100).atEndOfMonth()  // Adjust as needed
+        val daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.MONDAY)
+        weekCalendarView.setup(startDate, endDate, daysOfWeek.first())
+        weekCalendarView.scrollToWeek(currentDate)
 
         val titlesContainer = findViewById<ViewGroup>(R.id.titlesContainer)
         titlesContainer.children
-            .map { it as TextView } // TODO: questo è un errore
+            .map { it as TextView }
             .forEachIndexed { index, textView ->
                 val dayOfWeek = daysOfWeek[index]
                 val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
                 textView.text = title
             }
 
-        calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+        weekCalendarView.weekHeaderBinder = object : WeekHeaderFooterBinder<MonthViewContainer> {
             override fun create(view: View) = MonthViewContainer(view)
-            override fun bind(container: MonthViewContainer, data: CalendarMonth) {
+
+            override fun bind(container: MonthViewContainer, data: Week) {
                 // Remember that the header is reused so this will be called for each month.
                 // However, the first day of the week will not change so no need to bind
                 // the same view every time it is reused.
                 if (container.titlesContainer.tag == null) {
-                    container.titlesContainer.tag = data.yearMonth
+                    // container.titlesContainer.tag = data.yearMonth
                     container.titlesContainer.children.map { it as TextView }
                         .forEachIndexed { index, textView ->
                             val dayOfWeek = daysOfWeek[index]
@@ -80,6 +82,9 @@ class CalendarActivity : AppCompatActivity() {
 
     class DayViewContainer(view: View) : ViewContainer(view) {
         val textView = view.findViewById<TextView>(R.id.calendarDayText)
+
+        // With ViewBinding
+        // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
     }
 
     class MonthViewContainer(view: View) : ViewContainer(view) {

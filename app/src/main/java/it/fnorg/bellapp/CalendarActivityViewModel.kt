c857c42e0f9277958f8548d6a18f4a7a3dc3464a
N1C03BN0 <s5182167@studenti.unige.it>
2024-06-25 18:00:40 +0200

    Add event with errors

--- app/src/main/java/it/fnorg/bellapp/CalendarActivityViewModel.kt
+++ app/src/main/java/it/fnorg/bellapp/CalendarActivityViewModel.kt
@@ -6,78 +6,7 @@ import java.time.LocalDateTime
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
-data class Event(
-    val time: LocalDateTime,
-    val melody: Int,
-    val name: String,
-    @ColorRes val color: Int,
-)
 
-fun generateEvents(): List<Event> = buildList {
-    val currentMonth = YearMonth.now()
-
-    currentMonth.atDay(17).also { date ->
-        add(
-            Event(
-                date.atTime(10, 0),
-                3,
-                "Ave Maria",
-                R.color.naples
-            ),
-        )
-        add(
-            Event(
-                date.atTime(12, 30),
-                5,
-                "AC/DC",
-                R.color.lightred
-            ),
-        )
-        add(
-            Event(
-                date.atTime(13, 30),
-                5,
-                "AC/DC",
-                R.color.lightred
-            ),
-        )
-        add(
-            Event(
-                date.atTime(16, 30),
-                5,
-                "AC/DC",
-                R.color.lightred
-            ),
-        )
-        add(
-            Event(
-                date.atTime(17, 30),
-                1,
-                "Coldplay",
-                R.color.jade
-            ),
-        )
-        add(
-            Event(
-                date.atTime(21, 30),
-                2,
-                "Trilli",
-                R.color.jade
-            ),
-        )
-        add(
-            Event(
-                date.atTime(23, 30),
-                5,
-                "AC/DC",
-                R.color.lightred
-            ),
-        )
-    }
-}
-
-val eventDateTimeFormatter: DateTimeFormatter =
-    DateTimeFormatter.ofPattern("EEE'\n'dd MMM'\n'HH:mm")
 
 class CalendarActivityViewModel : ViewModel() {
 
